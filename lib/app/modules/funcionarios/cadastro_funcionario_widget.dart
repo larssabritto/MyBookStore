291855814c142a4547a0881709f7d690b9cda06b
larssabritto <89873299+larssabritto@users.noreply.github.com>
2023-05-07 21:44:48 -0300

    Cadastro e Deleção de Funcionário

--- lib/app/modules/funcionarios/cadastro_funcionario_widget.dart
+++ lib/app/modules/funcionarios/cadastro_funcionario_widget.dart
@@ -1,14 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:mybookstore/app/components/button_widget.dart';
 import 'package:mybookstore/app/components/input_text_widget.dart';
 import 'package:mybookstore/app/components/router_button_widget.dart';
+import 'package:mybookstore/app/dtos/default.response.dto.dart';
+import 'package:mybookstore/app/dtos/user_dto.dart';
 import 'package:mybookstore/app/modules/funcionarios/avatar_profile_widget.dart';
+import 'package:mybookstore/app/repository/contracts/iemployee.repository.dart';
+import 'package:mybookstore/app/repository/employee.repository.dart';
 import 'package:reactive_forms/reactive_forms.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CadastrarFuncionarioWidget extends StatelessWidget {
   final double bannerHeight = 200.0;
   final double avatarHeight = 90.0;
+  final IEmployeeRepository employeeRepository = EmployeeRepository();
 
   CadastrarFuncionarioWidget({Key? key}) : super(key: key);
+
+  Future<DefaultResponseDTO> cadastrarFuncionario() async {
+    try {
+      SharedPreferences preferences = await SharedPreferences.getInstance();
+
+      UserDTO storeEmployee = UserDTO(
+        name: form.control('nome').value,
+        password: form.control('senha').value,
+        username: form.control('usuario').value,
+        photo: "https://cdn.dribbble.com/users/160155/screenshots/1526505/media/617bae5c11b9b42021065f5a610001dc.png?compress=1&resize=400x300&vertical=top"
+      );
+
+      DefaultResponseDTO response = await employeeRepository.createStoreEmployee(preferences.getInt("idStore") ?? -1, storeEmployee);
+
+      Modular.to.navigate("/home/");
+      return response;
+    } catch (exception) {
+      print(exception);
+      return DefaultResponseDTO();
+    }
+  }
 
   FormGroup form = FormGroup({
     'nome': FormControl(validators: [
@@ -27,45 +56,42 @@ class CadastrarFuncionarioWidget extends StatelessWidget {
       Validators.maxLength(10),
     ]),
   });
-  
+
   @override
   Widget build(BuildContext context) {
     final double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      resizeToAvoidBottomInset: false,
-      body: ReactiveForm(
-        formGroup: form,
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 15,vertical: 10),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const AvatarProfileWidget(),
-                const SizedBox(height: 30),
-                Column(
-                  children: const [
-                    InputTextWidget(
-                        label: "Nome", formControlName: "nome"),
-                    SizedBox(height: 15),
-                    InputTextWidget(
-                        label: "Usuário  de login",
-                        formControlName: "usuario"),
-                    SizedBox(height: 15),
-                    InputTextWidget(
-                        label: "Senha", formControlName: "senha"),
-                    SizedBox(height: 15),
-                  ],
-                ),
-                const RouterButtonWidget(text: "Editar", route: ""),
-                const SizedBox(height: 15),
-                const RouterButtonWidget(text: "Cancelar", route: ""),
-              ],
+        resizeToAvoidBottomInset: false,
+        body: ReactiveForm(
+          formGroup: form,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const AvatarProfileWidget(),
+                  const SizedBox(height: 30),
+                  Column(
+                    children: const [
+                      InputTextWidget(label: "Nome", formControlName: "nome"),
+                      SizedBox(height: 15),
+                      InputTextWidget(
+                          label: "Usuário  de login",
+                          formControlName: "usuario"),
+                      SizedBox(height: 15),
+                      InputTextWidget(label: "Senha", formControlName: "senha"),
+                      SizedBox(height: 15),
+                    ],
+                  ),
+                  ButtonWidget(text: "Editar", onPressed: () => cadastrarFuncionario(),),
+                  const SizedBox(height: 15),
+                  const RouterButtonWidget(text: "Cancelar", route: ""),
+                ],
+              ),
             ),
           ),
-        ),
-      )
-    );
+        ));
   }
 }
--- lib/app/modules/main/views/funcionario/components/funcionario_list_item_widget.dart
+++ lib/app/modules/main/views/funcionario/components/funcionario_list_item_widget.dart
@@ -1,12 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:mybookstore/app/dtos/employee/get_employee/get_employee.response.dto.dart';
+import 'package:mybookstore/app/repository/contracts/iemployee.repository.dart';
+import 'package:mybookstore/app/repository/employee.repository.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class FuncionarioListItem extends StatelessWidget {
   final GetEmployeeResponseDTO employee;
-  const FuncionarioListItem({
+  final IEmployeeRepository employeeRepository = EmployeeRepository();
+
+  FuncionarioListItem({
     Key? key,
     required this.employee,
   }) : super(key: key);
+
+  Future<void> deleteEmployee() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+
+    await employeeRepository.deleteStoreEmployee(preferences.getInt("idStore") ?? -1, employee.id ?? -1);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +41,7 @@ class FuncionarioListItem extends StatelessWidget {
               icon: const Icon(Icons.edit_rounded),
             ),
             IconButton(
-              onPressed: () {},
+              onPressed: () => deleteEmployee(),
               icon: const Icon(
                 Icons.delete_outline_rounded,
               ),
